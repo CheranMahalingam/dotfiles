dd3f57692e75cb9a33c908d48f81278a7fa7093c
Cheran Mahalingam <cheranm@outlook.com>
2023-05-01 03:00:52 -0400

    chore: migrate to on_attach for nvim-tree

--- .config/nvim/lua/config/plugins/nvim-tree.lua
+++ .config/nvim/lua/config/plugins/nvim-tree.lua
@@ -6,25 +6,22 @@ local M = {
   },
 }
 
+local function on_attach(bufnr)
+  local api = require("nvim-tree.api")
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  vim.keymap.set("n", "<S-Right>", api.node.open.vertical, opts("Open: Vertical Split"))
+  vim.keymap.set("n", "<S-Down>", api.node.open.horizontal, opts("Open: Horizontal Split"))
+end
+
 function M.config()
   require("nvim-tree").setup({
-    view = {
-      mappings = {
-        list = {
-          { key = {"<S-Right>"}, action = "vsplit", mode = "n" },
-          { key = {"<S-Down>"}, action = "split", mode = "n" },
-        },
-      },
-    },
-    git = {
-      enable = true,
-      ignore = false,
-    },
-    renderer = {
-      highlight_opened_files = "none",
-      highlight_git = false,
-    },
-    respect_buf_cwd = true,
+    on_attach = on_attach,
   })
 end
 

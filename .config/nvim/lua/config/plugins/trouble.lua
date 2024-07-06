def7e54c769fdee2d527f3ef0cfa7bdd0d1e3bce
Cheran Mahalingam <cheranm@outlook.com>
2024-07-05 20:05:11 -0700

    chore: update trouble to use v3 api

--- .config/nvim/lua/config/plugins/trouble.lua
+++ .config/nvim/lua/config/plugins/trouble.lua
@@ -1,14 +1,26 @@
 local M = {
   "folke/trouble.nvim",
-  event = "BufReadPre",
+  opts = {},
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>tt",
+      "<cmd>Trouble toggle<cr>",
+    },
+    {
+      "<leader>tw",
+      "<cmd>Trouble diagnostics toggle<cr>",
+    },
+    {
+      "<leader>td",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+    },
+    {
+      "<leader>tl",
+      "<cmd>Trouble lsp toggle focus=false<cr>",
+    },
+  },
 }
-
-function M.init()
-  vim.keymap.set("n", "<leader>tt", "<cmd>TroubleToggle<cr>")
-  vim.keymap.set("n", "<leader>tw", "<cmd>TroubleToggle workspace_diagnostics<cr>")
-  vim.keymap.set("n", "<leader>td", "<cmd>TroubleToggle document_diagnostics<cr>")
-  vim.keymap.set("n", "<leader>tf", "<cmd>TroubleToggle quickfix<cr>")
-end
 
 return M
 
